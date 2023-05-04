016eb94ec533e1173a8081daa01ea6ec30448d40
therundmc <antoine.cavallera@gmail.com>
2023-05-05 00:52:18 +0200

    refactor

--- sys_info.py
+++ sys_info.py
@@ -6,19 +6,24 @@ from luma.core.render import canvas
 from luma.oled.device import ssd1306
 from PIL import ImageFont, ImageDraw, Image
 
+
+# Initialize OLED device
 serial = i2c(port=0, address=0x3C)
 device = ssd1306(serial, rotate=2)
 
+
+FONT_PATH = "/usr/local/FreePixel.ttf"
+
+
 def draw_line_graph(values, width, height):
+    """Draw a line graph on a PIL image."""
     # Create a new image with a black background
     img = Image.new('1', (width, height), color=0)
     draw = ImageDraw.Draw(img)
 
     # Determine the x and y scaling factors to fit the values within the image size
-    if (len(values) - 1 == 0):
-        return
-    if (max(values) == 0):
-        return
+    if len(values) < 2 or max(values) == 0:
+        return None
     x_scale = width / (len(values) - 1)
     y_scale = height / max(values)
 
@@ -32,79 +37,56 @@ def draw_line_graph(values, width, height):
 
     return img
 
-def draw_cpu():
-    cpu_values = []
+
+def draw_stat(stat_func, label):
+    """Draw a stat (CPU usage, RAM usage, temperature) on the OLED display."""
+    values = []
     start_time = time.time()
+
     while time.time() - start_time < 10:
-        cpu_percent = psutil.cpu_percent(interval=1)
-        cpu_values.append(cpu_percent)
+        value = stat_func()
+        if value is not None:
+            values.append(value)
+
         with canvas(device) as draw:
-            font_percent = ImageFont.truetype('/usr/local/FreePixel.ttf', 40)
-            font_label = ImageFont.truetype('/usr/local/FreePixel.ttf', 14)
-            label = "CPU"
+            font_percent = ImageFont.truetype(FONT_PATH, 40)
+            font_label = ImageFont.truetype(FONT_PATH, 14)
             label_width, _ = draw.textbbox((0, 0), label, font=font_label)[2:]
             draw.text((0, 50), label, font=font_label, fill=1)
-            draw.text((label_width, 10), f"{cpu_percent}%", font=font_percent, fill=1)
+            draw.text((label_width + 1, 10), f"{value:.1f}%", font=font_percent, fill=1)
 
-            if len(cpu_values) > 1:
-                # Draw line graph of CPU values
-                graph_width = 95
-                graph_height = 10
-                graph_img = draw_line_graph(cpu_values, graph_width, graph_height)
-                draw.bitmap((33, 54), graph_img, fill=1)     
-
-
-def draw_ram():
-    ram_values = []
-    start_time = time.time()    
-    while time.time() - start_time < 10:
-        svmem = psutil.virtual_memory()
-        ram_used = svmem.used/(1024*1024)
-        ram_total = svmem.total/(1024*1024)
-        ram_percent = svmem.percent
-        ram_values.append(ram_percent)
-        with canvas(device) as draw:
-            font_percent = ImageFont.truetype('/usr/local/FreePixel.ttf', 40)
-            font_label = ImageFont.truetype('/usr/local/FreePixel.ttf', 14)
-            label = "RAM"
-            label_width, _ = draw.textbbox((0, 0), label, font=font_label)[2:]
-            draw.text((0, 50), label, font=font_label, fill=1)
-            draw.text((label_width + 1, 10), f"{ram_percent}%", font=font_percent, fill=1)
-
-            if len(ram_values) > 1:
-                # Draw line graph of CPU values
+            if len(values) > 1:
+                # Draw line graph of values
                 graph_width = 100
                 graph_height = 10
-                graph_img = draw_line_graph(ram_values, graph_width, graph_height)
-                draw.bitmap((33, 54), graph_img, fill=1)
+                graph_img = draw_line_graph(values, graph_width, graph_height)
+                if graph_img is not None:
+                    draw.bitmap((33, 54), graph_img, fill=1)
 
-def draw_temp():
-    temp_values = []
-    start_time = time.time()
-    while time.time() - start_time < 10:
-        temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
-        temp_values.append(temp)
-        with canvas(device) as draw:
-            font_percent = ImageFont.truetype('/usr/local/FreePixel.ttf', 40)
-            font_label = ImageFont.truetype('/usr/local/FreePixel.ttf', 14)
-            label = "TMP"
-            label_width, _ = draw.textbbox((0, 0), label, font=font_label)[2:]
-            draw.text((0, 50), label, font=font_label, fill=1)
-            draw.text((label_width, 10), f"{temp}C", font=font_percent, fill=1)
 
-            if len(temp_values) > 1:
-                # Draw line graph of temperature values
-                graph_width = 100
-                graph_height = 10
-                graph_img = draw_line_graph(temp_values, graph_width, graph_height)
-                draw.bitmap((33, 54), graph_img, fill=1)
+def get_cpu_percent():
+    """Get the CPU usage percentage."""
+    return psutil.cpu_percent(interval=1)
+
+
+def get_ram_percent():
+    """Get the RAM usage percentage."""
+    svmem = psutil.virtual_memory()
+    return svmem.percent
+
+
+def get_temperature():
+    """Get the CPU temperature in degrees Celsius."""
+    temps = psutil.sensors_temperatures().get('coretemp', [])
+    if temps:
+        return temps[0].current
 
 
 if __name__ == "__main__":
     try:
         while True:
-            draw_cpu()
-            draw_ram()
-            draw_temp()
+            draw_stat(get_cpu_percent, "CPU")
+            draw_stat(get_ram_percent, "RAM")
+            draw_stat(get_temperature, "TMP")
     except KeyboardInterrupt:
         pass
